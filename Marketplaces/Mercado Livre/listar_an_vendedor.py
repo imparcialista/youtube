b9b624f4142da52cebe973bd4005800ec3b90418
imparcialista <lucasaraujoz13@outlook.com>
2024-03-06 18:42:23 -0300

    +1000
    pegando mais de 1000 anúncios no Mercado Livre

--- Marketplaces/Mercado Livre/listar_an_vendedor.py
+++ Marketplaces/Mercado Livre/listar_an_vendedor.py
@@ -53,13 +53,8 @@ def fazer_reqs(pagina):
 
 
 def pegar_scroll_id():
-    if apenas_itens_ativos:
-        filtro = 'include_filters=true&status=active'
-    else:
-        filtro = ''
-
     url = (f'https://api.mercadolibre.com/users/'
-           f'{id_do_vendedor}/items/search?search_type=scan')
+           f'{id_do_vendedor}/items/search?search_type=scan&limit=100')
 
     payload = {}
     headers = {
@@ -74,13 +69,13 @@ def pegar_scroll_id():
 
 
     resposta = resposta.json()
-    print(resposta)     # teste
+    # print(resposta)     # teste
     return resposta
 
 
 def proxima_pagina(scroll):
     url = (f'https://api.mercadolibre.com/users/'
-           f'{id_do_vendedor}/items/search?search_type=scan&scroll_id={scroll}')
+           f'{id_do_vendedor}/items/search?search_type=scan&scroll_id={scroll}&limit=100')
 
     payload = {}
     headers = {
@@ -95,7 +90,7 @@ def proxima_pagina(scroll):
 
 
     resposta = resposta.json()
-    print(resposta)     # teste
+    # print(resposta)     # teste
     return resposta
 
 
@@ -115,7 +110,43 @@ def pegar_todos_produtos():
         print(separador)
         print(f'Quantidade de anúncios: {quantidade_de_an}')
 
-        quantidade_de_an = quantidade_de_an - 1000
+        while quantidade_de_an > 0:
+            quantidade_de_an -= 100
+            paginas += 1
+
+        print(f'Páginas para percorrer: {paginas}')
+        print(separador)
+        paginas = paginas - 1
+
+        lista_scroll = []
+
+        primeiro_scroll = pegar_scroll_id()
+        print(f'Produtos coletados: {len(primeiro_scroll['results'])}')
+
+        lista_scroll.append(primeiro_scroll['scroll_id'])
+        for produto in primeiro_scroll['results']:
+            lista.append(produto)
+
+
+        def gerar_scroll(scroll_anterior):
+            scroll = proxima_pagina(scroll_anterior)
+            print(f'Produtos coletados: {len(scroll['results'])}')
+
+            for produto in scroll['results']:
+                lista.append(produto)
+
+            lista_scroll.append(scroll['scroll_id'])
+
+        for pagina in range(paginas):
+            gerar_scroll(lista_scroll[pagina])
+
+
+    else:          
+        resposta = fazer_reqs(0)
+        quantidade_de_an = resposta['paging']['total']
+
+        print(separador)
+        print(f'Quantidade de anúncios: {quantidade_de_an}')
 
         while quantidade_de_an > 0:
             quantidade_de_an -= 50
@@ -124,49 +155,20 @@ def pegar_todos_produtos():
         print(f'Páginas para percorrer: {paginas}')
         print(separador)
 
-        lista_scroll = []
-
-        primeiro_scroll = pegar_scroll_id()
-        lista_scroll.append(primeiro_scroll['scroll_id'])
-
-
-        def gerar_scroll(scroll_anterior):
-            scroll = proxima_pagina(scroll_anterior)
-            lista_scroll.append(scroll['scroll_id'])
-            # return scroll
-
+        time.sleep(3)
 
         for pagina in range(paginas):
-            gerar_scroll(lista_scroll[pagina])
+            print(f'Página: {pagina + 1} | Offset {pagina * 50}')
+            resposta = fazer_reqs(pagina * 50)
 
-        print(separador)
-        return
+            # Aqui vem um resultado de 50 produtos
+            # Então vamos adicionar item por item na lista
+            for produto in resposta['results']:
+                lista.append(produto)
 
-    print(separador)
-    print(f'Quantidade de anúncios: {quantidade_de_an}')
-
-    while quantidade_de_an > 0:
-        quantidade_de_an -= 50
-        paginas += 1
-
-    print(f'Páginas para percorrer: {paginas}')
-    print(separador)
-
-    time.sleep(3)
-
-    for pagina in range(paginas):
-        print(f'Página: {pagina + 1} | Offset {pagina * 50}')
-        resposta = fazer_reqs(pagina * 50)
-
-        # Aqui vem um resultado de 50 produtos
-        # Então vamos adicionar item por item na lista
-        for produto in resposta['results']:
-            # print(i)
-            lista.append(produto)
-
-        # Eu recomendo colocar um tempo entre uma execução e outra
-        # Fazer muitas requisições em um curto período não é legal
-        time.sleep(1)
+            # Eu recomendo colocar um tempo entre uma execução e outra
+            # Fazer muitas requisições em um curto período não é legal
+            time.sleep(1)
 
     print(separador)
     time.sleep(1)
