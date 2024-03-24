dbcf23e4dc48b332dda35f75d01617202b60e671
imparcialista <lucasaraujoz13@outlook.com>
2024-03-24 00:40:22 -0300

    Update percorrer_itens.py

--- Marketplaces/Mercado Livre/exportar produtos/percorrer_itens.py
+++ Marketplaces/Mercado Livre/exportar produtos/percorrer_itens.py
@@ -1,30 +1,17 @@
 import pandas as pd
+
 from listar_todos_anuncios import *
 
 # caso aconteça o erro ao tentar gerar o arquivo excel
 # pip install openpyxl
 
-# Busca todos os produtos da conta selecionada
-pegar_produtos = False
-
 # Gera uma planilha com todos os produtos da conta selecionada
 gerar_os_arquivos = True
+lista_retorno = []
 
-if pegar_produtos:
-    lista_produtos = pegar_todos_produtos()
-    # mensagem(lista_produtos)
-
-    for inx_linha, linha in enumerate(lista_produtos):
-        # print(f'| {inx_linha} | {linha} |')
-        pass
-
-else:
-    referencia = open(f'Arquivos/{nome_da_conta}/{id_do_vendedor}-ids_mlb.txt', 'r')
-    lista_produtos = []
-
-    for linha in referencia:
-        linha = linha.replace(f'\n', "")
-        lista_produtos.append(linha)
+ids_mlb = f'Arquivos/{nome_da_conta}/{id_do_vendedor}-ids_mlb.json'
+if not os.path.exists(ids_mlb):
+    pegar_todos_produtos()
 
 
 def exportar_para_planilha(lista_json: list, colunas_drop: list):
@@ -48,7 +35,6 @@ def exportar_para_planilha(lista_json: list, colunas_drop: list):
 
     if os.path.exists(planilha):
         os.remove(planilha)
-        # mensagem('Arquivo de planilha antigo deletado')
 
     df.to_excel(planilha, index=False)
     mensagem(f'Planilha gerada')
@@ -56,12 +42,14 @@ def exportar_para_planilha(lista_json: list, colunas_drop: list):
 
 def gerar_arquivos():
     inicio_timer = time.time()
+    mensagem(f'Conta conectada: {nome_da_conta}')
     lista_geral = []
     gap_vinte = 0
     paginas = 0
 
     # defina qual o json de retorno você vai utilizar
-    df = pd.read_json(f'Arquivos/{nome_da_conta}/{id_do_vendedor}-ids_mlb.json')
+
+    df = pd.read_json(ids_mlb)
     quantidade_de_an = len(df)
 
     mensagem(f'Quantidade de anúncios: {quantidade_de_an}')
@@ -88,39 +76,37 @@ def gerar_arquivos():
 
         for grupo_de_itens in retorno:
             body = grupo_de_itens['body']
-            lista.append(body)
+
             atributos = body['attributes']
 
-            # lista_atributos = []
             for atributo in atributos:
                 if atributo['id'] == 'SELLER_SKU':
                     sku = atributo['values'][0]['name']
                     body['attributes'] = sku
                     break
-                    # lista_atributos.append(sku)
-                    # print(sku)
+
                 else:
                     body['attributes'] = ''
 
-    retorno = lista
+            lista_retorno.append(body)
 
     with open(f'Arquivos/{nome_da_conta}/{id_do_vendedor}-backup.txt', 'w') as documento_bkp:
-        documento_bkp.write(f'{lista_geral}\n')
+        documento_bkp.write(f'{lista_retorno}\n')
         mensagem('Arquivo backup.txt gerado')
 
-        drops = [
-            'site_id', 'official_store_id', 'user_product_id', 'seller_id', 'category_id', 'inventory_id',
-            'currency_id',
-            'sale_terms', 'buying_mode', 'listing_type_id', 'start_time', 'stop_time', 'end_time', 'expiration_time',
-            'thumbnail_id', 'pictures', 'video_id', 'descriptions', 'accepts_mercadopago',
-            'non_mercado_pago_payment_methods',
-            'international_delivery_mode', 'seller_address', 'seller_contact', 'location', 'geolocation',
-            'coverage_areas',
-            'warnings', 'listing_source', 'variations', 'sub_status', 'warranty', 'parent_item_id',
-            'differential_pricing',
-            'deal_ids', 'automatic_relist', 'start_time', 'stop_time', 'end_time', 'expiration_time']
+    drops = [
+        'site_id', 'official_store_id', 'user_product_id', 'seller_id', 'category_id', 'inventory_id',
+        'currency_id',
+        'sale_terms', 'buying_mode', 'listing_type_id', 'start_time', 'stop_time', 'end_time', 'expiration_time',
+        'thumbnail_id', 'pictures', 'video_id', 'descriptions', 'accepts_mercadopago',
+        'non_mercado_pago_payment_methods',
+        'international_delivery_mode', 'seller_address', 'seller_contact', 'location', 'geolocation',
+        'coverage_areas',
+        'warnings', 'listing_source', 'variations', 'sub_status', 'warranty', 'parent_item_id',
+        'differential_pricing',
+        'deal_ids', 'automatic_relist', 'start_time', 'stop_time', 'end_time', 'expiration_time']
 
-        exportar_para_planilha(retorno, drops)
+    exportar_para_planilha(lista_retorno, drops)
 
     # Fim do programa
     fim_timer = time.time()
