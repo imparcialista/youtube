a657d719f1c1c4b264a02f436f1fa539dab6802d
imparcialista <lucasaraujoz13@outlook.com>
2024-03-25 09:03:58 -0300

    Update filtrar_produtos.py

--- marketplaces/Mercado Livre/exportar produtos/filtrar_produtos.py
+++ marketplaces/Mercado Livre/exportar produtos/filtrar_produtos.py
@@ -1,5 +1,6 @@
-from percorrer_itens import *
+# from percorrer_itens import *
 
+'''
 planilha_arquivos = f'Arquivos/{nome_da_conta}/{id_do_vendedor}-planilha-produtos.xlsx'
 planilha_teste = 'C:/Users/Lucas/Downloads/planilha_teste.xlsx'
 
@@ -7,7 +8,8 @@ if not os.path.exists(planilha_arquivos):
     gerar_planilha()
     time.sleep(1)
 
-df_produtos = pd.read_excel(planilha_teste)
+# df_produtos = pd.read_excel(planilha_teste)
+df_produtos = pd.read_excel(planilha_arquivos)
 # print(df_produtos)
 
 for linha in df_produtos['Matriz']:
@@ -17,3 +19,28 @@ for linha in df_produtos['Matriz']:
     print(linha)
 
 print(df_produtos)
+'''
+
+sair = False
+print('Digite SAIR para encerrar o programa')
+
+
+def get_input():
+    input_user = input(str(''))
+    input_user = input_user.lower()
+    input_user = input_user.strip()
+    return input_user
+
+
+while not sair:
+    escolha = get_input()
+    
+    if escolha == 'sair':
+        print('Encerrando o programa...')
+        quit()
+    elif escolha == 'ajuda':
+        print('Digite SAIR para encerrar o programa')
+    else:
+        print('Opção inválida | Escolha uma das opções')
+
+    # print(input_user)
