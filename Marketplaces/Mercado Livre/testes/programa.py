d094c11c0754e48b795ba9613f5f37d0e1e840da
imparcialista <lucasaraujoz13@outlook.com>
2024-03-26 14:29:27 -0300

    programa

---
+++ Marketplaces/Mercado Livre/testes/programa.py
@@ -0,0 +1,403 @@
+import json
+import os
+import time
+import requests
+import pandas as pd
+
+
+# TODO Criar uma interface visual para gerenciamento dos anúncios
+
+# Caso aconteça o erro ao tentar gerar o arquivo excel: pip install openpyxl
+
+lista_retorno = []
+apenas_itens_ativos = False
+base = 'https://api.mercadolibre.com'
+lista = []
+sair = False
+
+
+'''
+FUNÇÕES
+'''
+
+
+def mensagem(texto):
+    print(f'{"-" * (len(texto) + 4)}\n| {texto} |')
+
+
+def fazer_reqs(url, access):
+    headers = {'Authorization': f'Bearer {access}'}
+    resposta = requests.get(f'{url}', headers=headers)
+
+    tentativa = 1
+    while tentativa < 12:
+        if resposta.status_code != 200:
+            if tentativa == 11:
+                mensagem('Número máximo de tentativas excedido')
+                quit()
+            else:
+                mensagem(f'Tentativa {tentativa} | Falha na requisição')
+                tentativa += 1
+                time.sleep(0.25)
+
+        else:
+            resposta = resposta.json()
+            return resposta
+
+
+def configurar_conta():
+    conta_configurada = False
+    while not conta_configurada:
+        access_token_value = input(str('\nInsira o Access Token = '))
+
+        headers = {'Authorization': f'Bearer {access_token_value}'}
+        resposta = requests.get(f'https://api.mercadolibre.com/users/me', headers=headers)
+
+        if resposta.status_code == 200:
+            return access_token_value
+        else:
+            print('Access Token inválido ou expirado')
+
+
+def pegar_nome_da_conta(access_token_value):
+    nome_retorno = fazer_reqs(f'{base}/users/me', access_token_value)
+    nome_conta = nome_retorno['nickname']
+    return nome_conta
+
+
+def pegar_scroll_id(access_token_value):
+    url = f'{base}/users/{(access_token_value[-9:])}/items/search?search_type=scan&limit=100'
+    resposta = fazer_reqs(url, access_token_value)
+    return resposta
+
+
+def proxima_pagina(scroll, access_token_value):
+    url = f'{base}/users/{(access_token_value[-9:])}/items/search?search_type=scan&scroll_id={scroll}&limit=100'
+    resposta = fazer_reqs(url, access_token_value)
+    return resposta
+
+
+def pegar_todos_ids(access_token_value):
+    mensagem(f'Conta conectada: {pegar_nome_da_conta(access_token)}')
+    inicio_timer = time.time()
+    paginas = 0
+
+    if apenas_itens_ativos:
+        filtro = 'include_filters=true&status=active'
+    else:
+        filtro = ''
+
+    url = f'{base}/users/{(access_token_value[-9:])}/items/search?{filtro}&offset={0}'
+    resposta = fazer_reqs(url, access_token_value)
+
+    quantidade_de_an = resposta['paging']['total']
+
+    if quantidade_de_an == 0:
+        mensagem('Nenhum anúncio ativo\nPrograma finalizado')
+        return
+
+    if quantidade_de_an > 1000:
+        mensagem(f'Quantidade de anúncios: {quantidade_de_an}')
+
+        while quantidade_de_an > 0:
+            quantidade_de_an -= 100
+            paginas += 1
+
+        mensagem(f'Páginas para percorrer: {paginas}')
+        paginas = paginas - 1
+
+        lista_scroll = []
+        primeiro_scroll = pegar_scroll_id(access_token_value)
+        # mensagem(f'IDs coletados: {len(primeiro_scroll["results"])}')
+        mensagem('Coletando IDs, por favor aguarde...')
+
+        lista_scroll.append(primeiro_scroll['scroll_id'])
+        for produto in primeiro_scroll['results']:
+            lista.append(produto)
+
+
+        def gerar_scroll(scroll_anterior):
+            scroll = proxima_pagina(scroll_anterior, access_token_value)
+            # mensagem(f'IDs coletados: {len(scroll["results"])}')
+
+            for id_mlb in scroll['results']:
+                lista.append(id_mlb)
+
+            lista_scroll.append(scroll['scroll_id'])
+
+        for pagina in range(paginas):
+            gerar_scroll(lista_scroll[pagina])
+
+    else:
+        quantidade_de_an = resposta['paging']['total']
+
+        mensagem(f'Quantidade de anúncios: {quantidade_de_an}')
+
+        while quantidade_de_an > 0:
+            quantidade_de_an -= 50
+            paginas += 1
+
+        mensagem(f'Páginas para percorrer: {paginas}')
+
+        for pagina in range(paginas):
+            url = f'{base}/users/{id_do_vendedor}/items/search?{filtro}&offset={pagina * 50}'
+
+            mensagem(f'Página: {pagina + 1} | Offset {pagina * 50}')
+
+            resposta = fazer_reqs(url, access_token_value)
+
+            for produto in resposta['results']:
+                lista.append(produto)
+
+    if not os.path.exists(f'Arquivos/{pegar_nome_da_conta(access_token)}'):
+        os.makedirs(f'Arquivos/{pegar_nome_da_conta(access_token)}')
+        mensagem(f'Pasta {pegar_nome_da_conta(access_token)} criada')
+
+    arquivo_json = f'Arquivos/{pegar_nome_da_conta(access_token)}/{id_do_vendedor}-ids_mlb.json'
+
+    if os.path.exists(arquivo_json):
+        os.remove(arquivo_json)
+
+    with open(arquivo_json, 'w') as outfile:
+        json.dump(lista, outfile)
+
+    fim_timer = time.time()
+    mensagem(f'{pegar_nome_da_conta(access_token)}: Todos os IDS foram coletados ')
+    mensagem(f'Tempo de execução: {fim_timer - inicio_timer} segundos')
+
+    return lista
+
+
+'''
+PROGRAMA GERAR PLANILHA
+'''
+
+
+def exportar_para_planilha(lista_json: list, colunas_drop: list, access_token_value):
+    arquivo_json = f'Arquivos/{(pegar_nome_da_conta(access_token))}/{(access_token_value[-9:])}-retorno-produtos.json'
+
+    if os.path.exists(arquivo_json):
+        os.remove(arquivo_json)
+
+    with open(arquivo_json, 'w') as outfile:
+        json.dump(lista_json, outfile)
+
+    df = pd.read_json(arquivo_json)
+    df = df.drop(colunas_drop, axis=1, errors='ignore')
+    planilha = f'Arquivos/{pegar_nome_da_conta(access_token)}/{id_do_vendedor}-planilha-produtos.xlsx'
+
+    if os.path.exists(planilha):
+        os.remove(planilha)
+
+    df['date_created'] = pd.to_datetime(df['date_created'], format='%d/%m/%Y %H:%M:%S')
+    df['last_updated'] = pd.to_datetime(df['last_updated'], format='%d/%m/%Y %H:%M:%S')
+    df = df.sort_values(by='last_updated', ascending=False)
+    df.to_excel(planilha, index=False)
+    mensagem(f'Planilha gerada')
+
+
+def gerar_planilha(access_token_value):
+    nome_da_conta = pegar_nome_da_conta(access_token_value)
+
+    ids_mlb = f'Arquivos/{nome_da_conta}/{(access_token_value[-9:])}-ids_mlb.json'
+    if not os.path.exists(ids_mlb):
+        pegar_todos_ids(access_token_value)
+
+    inicio_timer = time.time()
+    lista_geral = []
+    gap_vinte = 0
+    paginas = 0
+
+    df = pd.read_json(ids_mlb)
+    quantidade_de_an = len(df)
+
+    mensagem(f'Quantidade de anúncios: {quantidade_de_an}')
+
+    while quantidade_de_an > 0:
+        quantidade_de_an -= 20
+        paginas += 1
+
+    mensagem(f'Páginas para percorrer: {paginas}')
+
+    # with open(f'teste.txt', 'w') as documento:
+    for pagina in range(paginas):
+        inicio = gap_vinte
+        fim = gap_vinte + 20
+        itens = df[inicio:fim]
+        lista_concatenada = ','.join(itens[0])
+        lista_geral.append(lista_concatenada)
+        gap_vinte += 20
+
+    for i_pack, pack in enumerate(lista_geral):
+        mensagem(f'Página: {i_pack + 1} de {paginas}')
+        url = f'https://api.mercadolibre.com/items?ids={pack}'
+        retorno = fazer_reqs(url, access_token_value)
+
+        for grupo_de_itens in retorno:
+            body = grupo_de_itens['body']
+
+            envio = body['shipping']['logistic_type']
+            if envio == 'cross_docking':
+                body['shipping'] = 'Normal'
+            elif envio == 'fulfillment':
+                body['shipping'] = 'Full'
+            elif envio == 'not_specified':
+                body['shipping'] = 'Não especificado'
+            else:
+                pass
+
+            atributos = body['attributes']
+
+            for atributo in atributos:
+                if atributo['id'] == 'SELLER_SKU':
+                    sku = atributo['values'][0]['name']
+                    body['attributes'] = sku
+                    break
+
+                else:
+                    body['attributes'] = ''
+
+            if body['status'] == 'active':
+                body['status'] = 'Ativo'
+
+            elif body['status'] == 'paused':
+                body['status'] = 'Pausado'
+
+            elif body['status'] == 'closed':
+                body['status'] = 'Fechado'
+
+            elif body['status'] == 'under_review':
+                body['status'] = 'Sob revisão'
+            else:
+                pass
+
+            criado = body['date_created']
+            atua = body['last_updated']
+
+            body['date_created'] = f'{criado[8:10]}/{criado[5:7]}/{criado[0:4]} {criado[11:19]}'
+            body['last_updated'] = f'{atua[8:10]}/{atua[5:7]}/{atua[0:4]} {atua[11:19]}'
+
+            porcentagem = body['health']
+
+            try:
+                float(porcentagem)
+                body['health'] = f'{(float(porcentagem)) * 100}%'
+
+            except:
+                pass
+
+            if body['catalog_listing'] == 'TRUE':
+                body['catalog_listing'] = 'Verdadeiro'
+            else:
+                body['catalog_listing'] = 'Falso'
+
+            if len(body['item_relations']) == 0:
+                body['item_relations'] = 'Sem relação'
+            else:
+                body['item_relations'] = body['item_relations'][0]['id']
+
+            if len(body['channels']) == 2:
+                body['channels'] = 'Vendido em ambos canais'
+            elif body['channels'][0] == 'marketplace':
+                body['channels'] = 'Vendido apenas no Mercado Livre'
+            elif body['channels'][0] == 'mshops':
+                body['channels'] = 'Vendido apenas no Mercado Shops'
+            else:
+                pass
+
+            lista_retorno.append(body)
+
+    drops = [
+        'site_id', 'official_store_id', 'user_product_id', 'seller_id', 'category_id', 'inventory_id',
+        'currency_id',
+        'sale_terms', 'buying_mode', 'listing_type_id', 'start_time', 'stop_time', 'end_time', 'expiration_time',
+        'thumbnail_id', 'pictures', 'video_id', 'descriptions', 'accepts_mercadopago',
+        'non_mercado_pago_payment_methods',
+        'international_delivery_mode', 'seller_address', 'seller_contact', 'location', 'geolocation',
+        'coverage_areas',
+        'warnings', 'listing_source', 'variations', 'sub_status', 'warranty', 'parent_item_id',
+        'differential_pricing',
+        'deal_ids', 'automatic_relist', 'start_time', 'stop_time', 'end_time', 'expiration_time', 'condition',
+        'seller_custom_field']
+
+    exportar_para_planilha(lista_retorno, drops, access_token_value)
+
+    # Fim do programa
+    fim_timer = time.time()
+    mensagem(f"Programa: Atualizar planilha finalizado | Tempo de execução: {fim_timer - inicio_timer} segundos")
+
+
+'''
+PROGRAMA FINAL
+'''
+
+
+mensagem_base = (
+    '\n[*] Escolha uma das opções, número ou comando'
+    '\n[1] Atualizar planilha | Gera uma nova planilha com todos os produtos'
+    '\n[2] Atualizar ids | Busca e atualiza a lista de IDS do Mercado Livre'
+    '\n[3] Trocar de conta | Altere a conta conectada alterando o Access Token'
+    '\n[4] Abrir planilha | Abre a planilha de produtos da conta selecionada'
+                 )
+
+print(mensagem_base)
+
+access_token = configurar_conta()
+mensagem(f'Conta conectada: {pegar_nome_da_conta(access_token)}')
+mensagem('Digite SAIR para encerrar o programa')
+print(mensagem_base)
+
+
+def get_input():
+    input_user = input(str('> '))
+    input_user = input_user.lower()
+    input_user = input_user.strip()
+    return input_user
+
+
+while not sair:
+    escolha = get_input()
+
+    if escolha == 'sair':
+        mensagem('Encerrando o programa...')
+        quit()
+
+    elif escolha == '?' or escolha == 'ajuda':
+        mensagem('Digite SAIR para encerrar o programa')
+        mensagem(f'Conta conectada: {pegar_nome_da_conta(access_token)}')
+        print(mensagem_base)
+
+    elif escolha == '1' or escolha == 'atualizar planilha':
+        gerar_planilha(access_token)
+        mensagem(f'Conta conectada: {pegar_nome_da_conta(access_token)}')
+        print(mensagem_base)
+
+    elif escolha == '2' or escolha == 'atualizar ids':
+        pegar_todos_ids(access_token)
+        mensagem(f'Conta conectada: {pegar_nome_da_conta(access_token)}')
+        print(mensagem_base)
+
+    elif escolha == '3' or escolha == 'trocar de conta':
+        access_token = configurar_conta()
+        mensagem(f'Conta conectada: {pegar_nome_da_conta(access_token)}')
+        print(mensagem_base)
+
+    elif escolha == '4' or escolha == 'abrir planilha':
+        id_do_vendedor = access_token[-9:]
+        path = f'Arquivos/{(pegar_nome_da_conta(access_token))}/{id_do_vendedor}-planilha-produtos.xlsx'
+        path = os.path.realpath(path)
+
+        if not os.path.exists(path):
+            mensagem('A planilha ainda não existe, gerando a planilha...')
+            gerar_planilha(access_token)
+
+        mensagem('Abrindo o arquivo...')
+        os.startfile(path)
+        mensagem('Arquivo aberto')
+        mensagem(f'Conta conectada: {pegar_nome_da_conta(access_token)}')
+        print(mensagem_base)
+
+    else:
+        print('\n[X] Opção inválida | Escolha uma das opções')
+        mensagem(f'Conta conectada: {pegar_nome_da_conta(access_token)}')
+        print(mensagem_base)
